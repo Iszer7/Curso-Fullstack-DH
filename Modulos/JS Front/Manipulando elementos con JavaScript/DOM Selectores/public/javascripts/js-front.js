b16827b69778f97998fffb4545f49365b9a7dd2b
dkippes <kippes.diego@gmail.com>
2021-01-10 21:18:39 -0300

    dom selectores - js front

---
+++ Modulos/JS Front/Manipulando elementos con JavaScript/DOM Selectores/public/javascripts/js-front.js
@@ -0,0 +1,19 @@
+let elH1 = document.querySelector("h1"); //Devuelve un nodo
+console.log(elH1);
+
+//Se puede capturar a traves de una clase
+let navBar = document.querySelector(".nav-bar"); //Si no existe esa clase devuelve "null"
+console.log(navBar);
+
+//Capturar a todos los elementos que coincidan
+let losParrafos = document.querySelectorAll("p"); //Devuelve un NodeList -> es como un array
+console.log(losParrafos);
+
+//Recorremos todos los parrafos
+for (const unParrafo of losParrafos) {
+    console.log(unParrafo);
+}
+
+//Agarrando el strong de un parrafo
+let strong = document.querySelector('.parrafo-especial strong');
+console.log(strong);
---
+++ Modulos/JS Front/Manipulando elementos con JavaScript/DOM Selectores/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+let mainController = require('../controllers/mainController');
+
+/* GET home page. */
+router.get('/', mainController.index);
+
+router.get('/contacto', mainController.contact);
+
+module.exports = router;
