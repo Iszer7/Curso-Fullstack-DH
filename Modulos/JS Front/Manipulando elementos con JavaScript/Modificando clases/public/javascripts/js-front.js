48912035458cc8218cf7bd54d6a8d5537c55a4c3
dkippes <kippes.diego@gmail.com>
2021-01-10 23:08:45 -0300

    modificando clases js front

--- Modulos/JS Front/Manipulando elementos con JavaScript/Modificando clases/public/javascripts/js-front.js
+++ Modulos/JS Front/Manipulando elementos con JavaScript/Modificando clases/public/javascripts/js-front.js
@@ -1,12 +1,23 @@
 let titulo = document.querySelector("h1");
-let confirmaCambios = confirm('¿Queres cambiar el color del titulo?');
+titulo.innerHTML += " Soy un contenido que se agrega al final desde JS" //Si le pongo solo " = ", reemplaza todo
 
-if (confirmaCambios) {
+//classList - No devuelve nada si no tiene ninguna clase
+console.log(titulo.classList);
 
-    //Modifico el contenido
-    titulo.innerHTML += " Soy un contenido que se agrega al final desde JS" //Si le pongo solo " = ", reemplaza todo
+//Objeto classList - Agregar una clase que esta en el style
+titulo.classList.add('titulo-destacado');
 
-    //Cambiando el estilo
-    titulo.style.color = 'crimson';
-    titulo.style.fontSize = '50px';
+//Objeto classList - Sacar una clase
+let confirmarEliminarClase = confirm('Queres eliminar la clase del titulo?');
+
+if (confirmarEliminarClase) {
+    //La clase se elimina del h1, entonces no va aparecer en el html
+    titulo.classList.remove('titulo-destacado');
 }
+
+//Objeto classList - Toggle -> Sirve para Agregar o Sacar una clase preguntando.. existe esta clase? Si existe no la agrega sino la agrega
+//titulo.classList.toggle('titulo-destacado');
+
+
+//Si el elemento contine una clase
+titulo.classList.contains("titulo-destacado"); //Devuelve true o false
