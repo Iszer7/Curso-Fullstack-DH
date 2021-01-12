0b9f5b368b02e24a0410e01f17d7affc93c2db82
dkippes <kippes.diego@gmail.com>
2021-01-11 21:22:47 -0300

    eventos de mouse

--- Modulos/JS Front/Agregando interaccion con Eventos/Eventos de Mouse/script.js
+++ Modulos/JS Front/Agregando interaccion con Eventos/Eventos de Mouse/script.js
@@ -1,4 +1,16 @@
 window.addEventListener("load", function() {
 
-    
+    let botones = document.querySelectorAll(".w3-button");
+
+    //Recorro todos los botones
+    for (let i=0; i < botones.length; i++) {
+        //click => Se ejecuta al clickear
+        //mouseover => Se ejecuta apenas pasa el mouse por arriba
+        //mouseout => Se ejecuta apenas se va del eleymento
+        botones[i].addEventListener("mouseover", function() {
+            //this hace referencia al lugar donde fue el evento, poner botones[i] no funcionaria
+            this.style.color = prompt("Elija su color preferido (ingles)");
+        })
+    }
+
 })
