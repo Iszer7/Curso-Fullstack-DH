6ba2bc27c994ef134f265946c20e624eaa88a25c
dkippes <kippes.diego@gmail.com>
2021-02-12 23:38:47 -0300

    Statefull components - state setState

---
+++ Modulos/React/El ciclo de vida de los componentes/curso-react/src/components/Contador.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Contador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: props.inicial,
+    };
+  }
+
+  incrementar() {
+    this.setState({ numero: this.state.numero + 1 });
+  }
+
+  decrementar() {
+    this.setState({ numero: this.state.numero - 1 });
+  }
+
+  render() {
+      console.log('estoy renderizando');
+    return (
+      <div>
+        <p>Soy el numero {this.state.numero} </p>
+        <button onClick={() => this.incrementar()}>Incrementar</button>
+        <button onClick={() => this.decrementar()}>Decrementar</button>
+      </div>
+    );
+  }
+}
+
+export default Contador;
